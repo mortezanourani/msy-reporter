438ba59257444c0cd2a69290b3320f97e7950e27
Morteza Nourani <mortezanourani@gmail.com>
2025-01-06 22:32:16 +0330

    Add BuildingOwnership model
    - BuildingOwnership model added and BuildingOwnerships pages.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,4 +7,6 @@ namespace Reporter.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Gender> Genders { get; set; }
+
+    public DbSet<BuildingOwnership> BuildingOwnerships { get; set; }
 }
---
+++ Models/BuildingOwnership.cs
@@ -0,0 +1,8 @@
+﻿namespace Reporter.Models;
+
+public class BuildingOwnership
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+}
